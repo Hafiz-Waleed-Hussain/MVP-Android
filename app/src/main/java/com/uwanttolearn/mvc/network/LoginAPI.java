236771fb75991682ec8dfff3e43e698ef6ea3029
Hafiz <hafizwaleedhussain@gmail.com>
2015-11-29 18:35:36 +0800

    Our code ready to refactor

---
+++ app/src/main/java/com/uwanttolearn/mvc/network/LoginAPI.java
@@ -0,0 +1,16 @@
+package com.uwanttolearn.mvc.network;
+
+/**
+ * Created by hafiz on 29/11/2015.
+ */
+public class LoginAPI {
+
+    private final String USERNAME = "hafiz";
+    private final String PASSWORD = "123456";
+
+    public boolean authenticate(String username, String password){
+        if(USERNAME.equals(username) && PASSWORD.equals(password))
+            return true;
+        return false;
+    }
+}
